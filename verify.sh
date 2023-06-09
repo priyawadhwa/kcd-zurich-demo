5e98f76f784fcddd626508b87129e58e9dedc380
Priya Wadhwa <priya@chainguard.dev>
2023-06-08 19:41:45 -0700

    switch to trivy

--- verify.sh
+++ verify.sh
@@ -3,3 +3,8 @@
 cosign verify ghcr.io/priyawadhwa/nginx \
     --certificate-identity "https://github.com/priyawadhwa/kcd-zurich-demo/.github/workflows/build-and-sign.yaml@refs/heads/main" \
     --certificate-oidc-issuer "https://token.actions.githubusercontent.com"
+
+cosign verify-attestation ghcr.io/priyawadhwa/nginx-cves \
+    --certificate-identity "https://github.com/priyawadhwa/kcd-zurich-demo/.github/workflows/cves.yaml@refs/heads/main" \
+    --certificate-oidc-issuer "https://token.actions.githubusercontent.com" \
+    --type vuln
